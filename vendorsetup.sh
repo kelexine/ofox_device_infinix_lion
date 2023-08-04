b4e6aa1c19d5d05529df7154d947c7caa60dc8d9
IMYdev <omranmansour096@gmail.com>
2023-08-04 11:18:03 +0200

    Change the codename

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-FDEVICE="X6816"
+FDEVICE="lion"
 
 fox_get_target_device() {
 local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
@@ -16,7 +16,7 @@ fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export TW_DEFAULT_LANGUAGE="en"
-    export OF_DEVICE_ALT="X6816,FULL-64"
+    export OF_DEVICE_ALT="lion"
 	export LC_ALL="C"
 	export OF_AB_DEVICE=1
 	export FOX_EXTREME_SIZE_REDUCTION=1
