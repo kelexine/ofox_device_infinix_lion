d6d9bf19b8a0193a4a0b29dd1492d45a883c347f
IMYdev <omranmansour096@gmail.com>
2023-12-15 22:17:26 +0200

    attempt to fix Internal storage mounting issues

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,7 +147,8 @@ BOARD_USES_MTK_HARDWARE := true
 ## TWRP-Specific configuration
 TW_THEME := portrait_hdpi
 TW_DEVICE_VERSION := IMY
-RECOVERY_SDCARD_ON_DATA := true
+# Test if this fixes the bug where it creates it's own "0" directory Inside of Internal Storage!!!
+#RECOVERY_SDCARD_ON_DATA := true
 TARGET_USES_MKE2FS := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 1200
